30c2159206a3c64466196f26af5a9c3beac91349
juyeongMoon888 <whoor2828@gmail.com>
2025-06-20 19:31:28 +0900

    feat: 로그인 API 컨트롤러 구현

--- src/main/java/com/mybank/atmweb/controller/AuthApiController.java
+++ src/main/java/com/mybank/atmweb/controller/AuthApiController.java
@@ -1,4 +1,50 @@
 package com.mybank.atmweb.controller;
 
+import com.mybank.atmweb.domain.User;
+import com.mybank.atmweb.dto.LoginRequest;
+import com.mybank.atmweb.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthApiController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public AuthApiController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request, HttpSession session) {
+        // 1. 아이디로 사용자 조회
+        User user = userRepository.findByLoginId(request.getLoginId())
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 사용자입니다."));
+
+        // 2. 비밀번호 검증
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))  {
+            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+        }
+
+        //3. 로그인 성공 -> 세션에 사용자 정보 저장
+        session.setAttribute("loginUser", user);
+
+        return ResponseEntity.ok("로그인 성공");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session) {
+        session.invalidate(); //세션 제거
+        return ResponseEntity.ok("로그아웃 완료");
+    }
+
 }
+
