5f5c90b0cc5cb1421cb3f09223016f88e4ad84d0
juyeongMoon888 <whoor2828@gmail.com>
2025-06-22 22:46:48 +0900

    feat: add JwtUtil class for generating and parsing JWT tokens

--- src/main/java/com/mybank/atmweb/security/JwtUtil.java
+++ src/main/java/com/mybank/atmweb/security/JwtUtil.java
@@ -3,14 +3,17 @@ package com.mybank.atmweb.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
-public class JwUtil {
+public class JwtUtil {
 
-    private static final String SECRET_KEY = "your-secret-key";
+    private static final String SECRET ="your-256-bit-secret-your-256-bit-secret";
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes()); //최소 32바이트 이상
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;//1시간
 
     public String createToken(Long userId, String role) {
@@ -18,10 +21,11 @@ public class JwUtil {
                 .setSubject(String.valueOf(userId))
                 .claim("role", role)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
+
     public Claims parseToken(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET_KEY)
