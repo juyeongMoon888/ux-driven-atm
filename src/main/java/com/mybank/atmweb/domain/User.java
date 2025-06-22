8383e7ad7d1df1f38a3c5e0c7e2e561a6242af1d
juyeongMoon888 <whoor2828@gmail.com>
2025-06-22 22:44:33 +0900

    feat: add role column to User entity with default USER

--- src/main/java/com/mybank/atmweb/domain/User.java
+++ src/main/java/com/mybank/atmweb/domain/User.java
@@ -21,6 +21,10 @@ public class User {
     private String password;//로그인 PW
     private String residentNumber; //주민등록번호
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
     //주인X 왜래키는 Account에 있으므로 Account가 연관관계의 주인이다.//관계를 실제로 저장,변경하는 쪽//User은 Account를 모른다.
     //User는 매핑된 거울일 뿐이다. //읽기 전용 //DB 반영 가능 불가
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL) //jakarta.persistence 가 표준임
