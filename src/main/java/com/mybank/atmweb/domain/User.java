e3a323dac2da252912e6ccff64bb49e56417b76f
juyeongMoon888 <whoor2828@gmail.com>
2025-06-24 20:05:25 +0900

    style: remove redundant comments

--- src/main/java/com/mybank/atmweb/domain/User.java
+++ src/main/java/com/mybank/atmweb/domain/User.java
@@ -25,10 +25,7 @@ public class User {
     @Column(nullable = false)
     private Role role = Role.USER;
 
-    //주인X 왜래키는 Account에 있으므로 Account가 연관관계의 주인이다.//관계를 실제로 저장,변경하는 쪽//User은 Account를 모른다.
-    //User는 매핑된 거울일 뿐이다. //읽기 전용 //DB 반영 가능 불가
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL) //jakarta.persistence 가 표준임
     private Account account;
 
-    //생성자, getter/setter, 비밀번호 체크 등
 }
