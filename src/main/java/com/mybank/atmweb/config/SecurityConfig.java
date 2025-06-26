f43bcbd7e455b55c5f91226619c65742cf2e7f00
juyeongMoon888 <whoor2828@gmail.com>
2025-06-26 13:31:30 +0900

    feat: register JwtAuthenticationFilter in the security filter chain

--- src/main/java/com/mybank/atmweb/config/SecurityConfig.java
+++ src/main/java/com/mybank/atmweb/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mybank.atmweb.config;
 
 import com.mybank.atmweb.handler.CustomLoginFailureHandler;
+import com.mybank.atmweb.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Autowired
     private CustomLoginFailureHandler customLoginFailureHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,9 +30,11 @@ public class SecurityConfig {
                 .csrf(csrf->csrf.disable())
 
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/", "/login", "/signup", "/api/**", "/api/users/**","/css/**", "/js/**").permitAll() //메인 페이지 접근 허용
+                        .requestMatchers("/", "/login", "/signup", "/api/**", "/api/users/**","/css/**", "/js/**")
+                        .permitAll() //메인 페이지 접근 허용
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); //필터 등록
                 return http.build();
     }
 
