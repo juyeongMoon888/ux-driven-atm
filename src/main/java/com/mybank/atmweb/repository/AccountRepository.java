c3865ce5e76156b64a5b22e33a8c669fed230338
juyeongMoon888 <whoor2828@gmail.com>
2025-06-29 19:34:23 +0900

    feat: Implement findByAccountNumber and findAllByUser in repository

--- src/main/java/com/mybank/atmweb/repository/AccountRepository.java
+++ src/main/java/com/mybank/atmweb/repository/AccountRepository.java
@@ -4,8 +4,11 @@ import com.mybank.atmweb.domain.Account;
 import com.mybank.atmweb.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, User> {
-    Optional<Account> findByUser(User user);
+    Optional<Account> findByAccountNumber(String accountNumber); //단일 계좌 조회
+
+    List<Account> findAllByUser(User user);//사용자 계좌 전체 조회
 }
