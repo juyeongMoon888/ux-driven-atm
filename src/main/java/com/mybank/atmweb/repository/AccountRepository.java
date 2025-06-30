2231bbff740ea4b2769458f718fb28ed2cda6944
juyeongMoon888 <whoor2828@gmail.com>
2025-06-30 11:44:35 +0900

    feat: add query to find first account for authenticated user

--- src/main/java/com/mybank/atmweb/repository/AccountRepository.java
+++ src/main/java/com/mybank/atmweb/repository/AccountRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface AccountRepository extends JpaRepository<Account, User> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(String accountNumber); //단일 계좌 조회
 
     List<Account> findAllByUser(User user);//사용자 계좌 전체 조회
+
+    Optional<Account> findFirstByUser_Id(Long userId);
 }
