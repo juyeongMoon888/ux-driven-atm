65073dc9821a906c4223873179b9676d37539343
juyeongMoon888 <whoor2828@gmail.com>
2025-07-08 23:27:51 +0900

    refactor: rename controller class to PageController

--- src/main/java/com/mybank/atmweb/controller/PageController.java
+++ src/main/java/com/mybank/atmweb/controller/PageController.java
@@ -1,24 +1,13 @@
 package com.mybank.atmweb.controller;
 
-import com.mybank.atmweb.domain.User;
-import com.mybank.atmweb.dto.SignupForm;
-import com.mybank.atmweb.repository.UserRepository;
 import com.mybank.atmweb.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+public class PageController {
 
     private final UserService userService;
 
