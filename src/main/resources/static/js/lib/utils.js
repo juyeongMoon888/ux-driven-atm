8853610554882e335203add45d3ef327852b27e7
juyeongMoon888 <whoor2828@gmail.com>
2025-07-12 22:19:49 +0900

    style: 오류 메시지 컬러 적용

--- src/main/resources/static/js/lib/utils.js
+++ src/main/resources/static/js/lib/utils.js
@@ -4,6 +4,31 @@ export function showValidationMessages(details) {
         const errorEl = document.getElementById(`${field}-error`);
         if (errorEl) {
             errorEl.textContent = message;
+            errorEl.style.color = "red";
         }
     }
+}
+
+export function getAccessToken() {
+    return localStorage.getItem("accessToken");
+}
+
+export function getUserFromLocalStorage() {
+    const userJson = localStorage.getItem("user");
+    return userJson ? JSON.parse(userJson) : null;
+}
+
+export function tryOnceToDetectRecovery() {
+    setTimeout(() => {
+        fetch("/api/ping")
+            .then(response => {
+                if (response.ok) {
+                localStorage.removeItem("accessToken");
+                location.reload();
+                }
+            })
+            .catch(() => {
+                console.warn("서버 여전히 꺼져 있음");
+            });
+    }, 5000);
 }
