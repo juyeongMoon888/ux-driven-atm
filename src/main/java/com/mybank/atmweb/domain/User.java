adfb04718642598896dc054b5acf86d879e34234
juyeongMoon888 <whoor2828@gmail.com>
2025-07-13 17:28:30 +0900

    fix: 회원가입 시 retryCount null 오류 방지를 위한 기본값 추가

--- src/main/java/com/mybank/atmweb/domain/User.java
+++ src/main/java/com/mybank/atmweb/domain/User.java
@@ -33,4 +33,11 @@ public class User {
     @Column(nullable = false)
     private Role role = Role.USER;
 
+    /**
+     * 로그인 실패 횟수를 저장하는 필드입니다.
+     * DB 컬럼이 NOT NULL 제약을 갖고 있어, 명시적으로 기본값 0을 설정해줍니다.
+     * 해당 설정이 없으면 회원가입 시 null 저장으로 인해 DataIntegrityViolationException이 발생할 수 있습니다.
+     */
+    @Column(nullable = false)
+    private int retryCount = 0;
 }
