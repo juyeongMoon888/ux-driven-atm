a785908dcb0fd9df72ef622a35560d3593f4f708
juyeongMoon888 <whoor2828@gmail.com>
2025-07-13 16:28:31 +0900

    feat: 회원가입 요청 DTO에 @Valid 유효성 검사 추가

--- src/main/java/com/mybank/atmweb/controller/UserApiController.java
+++ src/main/java/com/mybank/atmweb/controller/UserApiController.java
@@ -7,7 +7,6 @@ import com.mybank.atmweb.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,10 +29,11 @@ public class UserApiController {
 
     //회원가입 완료 후
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupForm form) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupForm form) {
         userService.signup(form);
-        return ResponseEntity
-                .ok(Map.of("message", "회원가입이 완료되었습니다."));
+        return ResponseEntity.ok(Map.of(
+                "code", "SIGNUP_SUCCESS",
+                "message", "회원가입이 완료되었습니다."));
     }
 
     @GetMapping("/me")
