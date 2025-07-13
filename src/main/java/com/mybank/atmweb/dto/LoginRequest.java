c8dac9b1fcaf5f046b6ee0b6871bf79af6bdf654
juyeongMoon888 <whoor2828@gmail.com>
2025-07-13 16:51:29 +0900

    feat: DTO 필드에 @NotBlank 유효성 검사 추가

--- src/main/java/com/mybank/atmweb/dto/LoginRequest.java
+++ src/main/java/com/mybank/atmweb/dto/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.mybank.atmweb.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank(message="아이디를 입력해 주세요.")
     private String loginId;
+    @NotBlank(message="비밀번호를 입력해 주세요.")
     private String password;
 }
