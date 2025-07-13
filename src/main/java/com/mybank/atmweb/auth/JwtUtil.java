94d896342dfb0d8c9f4f17769d015d6458a05490
juyeongMoon888 <whoor2828@gmail.com>
2025-07-13 16:08:49 +0900

    feat: JwtUtil에 리프레시 토큰 생성 및 토큰 관련 함수 추가

--- src/main/java/com/mybank/atmweb/auth/JwtUtil.java
+++ src/main/java/com/mybank/atmweb/auth/JwtUtil.java
@@ -1,37 +1,110 @@
 package com.mybank.atmweb.auth;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.mybank.atmweb.domain.User;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@RequiredArgsConstructor
 @Component
+@Slf4j
 public class JwtUtil {
 
     private static final String SECRET ="your-256-bit-secret-your-256-bit-secret";
-    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes()); //최소 32바이트 이상
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60;//1시간
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private static final long ACCESS_TOKEN_EXPIRE_MS = 1000 * 60 * 60;
+    private static final long REFRESH_TOKEN_EXPIRE_MS = 1000L * 60 * 60 * 24 * 7;
 
     //	로그인 성공 시 JWT 생성
-    public String createToken(Long id, String role) {
+    public String createAccessToken(User user) {
         return Jwts.builder()
-                .setSubject(String.valueOf(id))
-                .claim("role", role)
+                .setSubject("AccessToken")
+                .claim("userId", user.getId())
+                .claim("loginId", user.getLoginId())
+                .claim("role", user.getRole().name())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_MS))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(User user) {
+        return Jwts.builder()
+                .setSubject("RefreshToken")
+                .claim("userId", user.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRE_MS))
                 .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     //	필터에서 들어온 요청의 JWT 해석
-    public Claims parseToken(String token) {
-        return Jwts.parser()
+    public String getLoginIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
                 .setSigningKey(SECRET_KEY)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
+        return claims.get("loginId", String.class);
     }
+
+    public String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (SecurityException | MalformedJwtException e) {
+            log.warn("잘못된 JWT 서명: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.warn("만료된 토큰: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.get("userId", Long.class);
+    }
+
+    public Long getExpirationMillis(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Date expiration = claims.getExpiration();
+        return expiration.getTime() - System.currentTimeMillis();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
 }
