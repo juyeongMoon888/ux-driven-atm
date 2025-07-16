410a45d3d24b8c11db17c7d6903f422cb31572b8
juyeongMoon888 <whoor2828@gmail.com>
2025-07-16 11:48:12 +0900

    refactor: fetchWithAuth 내부에 handleErrorResponse 함수 연결

--- src/main/resources/static/js/lib/fetchWithAuth.js
+++ src/main/resources/static/js/lib/fetchWithAuth.js
@@ -1,12 +1,23 @@
 import { tryRefreshToken } from "./tryRefreshToken.js";
+import { handleErrorResponse } from "./utils.js";
 
 export async function fetchWithAuth(url, options = {}) {
     const token = localStorage.getItem("accessToken");
 
-    const res = await fetch(url, {
+    if (!token || token === "null" || token === "undefined") {
+        const newToken = await tryRefreshToken();
+        if (!newToken) {
+            localStorage.removeItem("accessToken");
+            window.location.href = "/login";
+            return null;
+        }
+        return fetchWithAuth(url, options);
+    }
+
+    let res = await fetch(url, {
         ...options,
         headers: {
-            (options.headers || {}),
+            ...(options.headers || {}),
             Authorization: `Bearer ${token}`
         }
     });
@@ -27,6 +38,10 @@ export async function fetchWithAuth(url, options = {}) {
             window.location.href = "/login";
             return null;
         }
+    }
+
+    if (!res.ok) {
+        await handleErrorResponse(res);
     }
     return res;
 }
