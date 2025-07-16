c8e47d46cf6a5b6cc2bd448ba357e364467fde3f
juyeongMoon888 <whoor2828@gmail.com>
2025-07-16 11:46:08 +0900

    feat: handleErrorResponse 공통 에러 처리 함수 추가

--- src/main/resources/static/js/lib/utils.js
+++ src/main/resources/static/js/lib/utils.js
@@ -32,3 +32,29 @@ export function tryOnceToDetectRecovery() {
             });
     }, 5000);
 }
+
+export async function handleErrorResponse(res) {
+    const contentType = res.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+        const errorData = await res.json();
+
+        if (res.status === 401) {
+            throw new ApiError(errorData.code, errorData.message);
+        } else if (res.status === 500) {
+            throw new Error ("서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+        } else {
+            throw new Error (`알 수 없는 오류 (code: ${status})`);
+        }
+    } else {
+        const text = await res.text();
+
+        if (res.status = 401) {
+            throw new Error(`인증 오류: ${text}`);
+        } else if (res.status === 500) {
+            throw new Error("서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+        } else {
+            throw new Error(`알 수 없는 오류 (code: $(res.status}) - ${text}`);
+        }
+    }
+}
