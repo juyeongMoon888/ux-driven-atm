c3b4f31ea204d54484857af843edf899eb5ca8aa
juyeongMoon888 <whoor2828@gmail.com>
2025-07-21 15:10:57 +0900

    refactor: TypeScript 문법 제거하여 ERROR_MESSAGES를 JS에서 사용 가능하게 수정

---
+++ src/main/resources/static/js/lib/constants/errorMessages.js
@@ -0,0 +1,20 @@
+export const ERROR_MESSAGES = {
+  NOTBLANK_LOGINID: "아이디를 입력해주세요",
+
+  NOTBLANK_PASSWORD: "비밀번호를 입력해주세요",
+  SIZE_PASSWORD: "비밀번호는 8자 이상이어야 합니다",
+
+  NOTBLANK_EMAIL: "이메일을 입력해주세요",
+  EMAIL_EMAIL: "이메일 형식이 올바르지 않습니다",
+
+  NOTBLANK_NAME: "이름을 입력해주세요",
+
+  NOTBLANK_RESIDENTNUMBER: "주민등록번호를 입력해주세요",
+  SIZE_RESIDENTNUMBER: "주민등록번호는 13자리 숫자여야 합니다",
+
+  NOTBLANK_GENDER: "성별을 입력해주세요",
+
+  NOTBLANK_PHONENUMBER: "전화번호를 입력해주세요",
+  PATTERN_PHONENUMBER: "전화번호는 10~11자리 숫자로 입력해주세요",
+  UNKNOWN_ERROR: "알 수 없는 오류가 발생했습니다"
+};
