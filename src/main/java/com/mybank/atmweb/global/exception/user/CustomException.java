62053e48e6a6bf126c2351ba2041bb562b00fc85
juyeongMoon888 <whoor2828@gmail.com>
2025-07-22 22:48:16 +0900

    refactor: CustomException 기반으로 통합하여 예외 재사용성 및 일관성 개선

---
+++ src/main/java/com/mybank/atmweb/global/exception/user/CustomException.java
@@ -0,0 +1,17 @@
+package com.mybank.atmweb.global.exception.user;
+
+import com.mybank.atmweb.global.exception.ErrorCode;
+
+public class CustomException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.name());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
