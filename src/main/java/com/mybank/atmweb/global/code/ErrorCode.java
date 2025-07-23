e0c359f8c46127d25a54557edd189b8508d68ef1
juyeongMoon888 <whoor2828@gmail.com>
2025-07-23 21:25:49 +0900

    refactor: ErrorCode에 BaseCode 인터페이스 적용

---
+++ src/main/java/com/mybank/atmweb/global/code/ErrorCode.java
@@ -0,0 +1,35 @@
+package com.mybank.atmweb.global.code;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode implements BaseCode{
+    INVALID_CREDENTIALS("error.invalid_credentials", HttpStatus.UNAUTHORIZED),
+    USER_EXIST("error.user_exist", HttpStatus.CONFLICT),
+    SERVER_ERROR("error.server_error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND("error.user_not_found", HttpStatus.UNAUTHORIZED),
+    DATA_INTEGRITY_VIOLATION("error.data_integrity_violation", HttpStatus.BAD_REQUEST),
+    VALIDATION_FAILED("error.validation_failed", HttpStatus.BAD_REQUEST),
+    TOKEN_EXPIRED("error.token_expired", HttpStatus.UNAUTHORIZED),
+    TOKEN_MALFORMED("error.token_malformed", HttpStatus.UNAUTHORIZED),
+    TOKEN_INVALID("error.token_invalid", HttpStatus.UNAUTHORIZED);
+
+    private final String messageKey;
+    private final HttpStatus httpStatus;
+
+
+    ErrorCode(String messageKey, HttpStatus httpStatus) {
+        this.messageKey = messageKey;
+        this.httpStatus = httpStatus;
+    }
+
+
+    @Override
+    public String getMessageKey() {
+        return messageKey;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
