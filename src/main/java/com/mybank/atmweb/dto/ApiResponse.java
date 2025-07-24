49cf5f760ca39563b35ae2ea60fb7afa2634095b
juyeongMoon888 <whoor2828@gmail.com>
2025-07-24 20:17:00 +0900

    fix: ApiResponse 필드에 @Getter 추가하여 Json 응답 값 누락 문제 해결

--- src/main/java/com/mybank/atmweb/dto/ApiResponse.java
+++ src/main/java/com/mybank/atmweb/dto/ApiResponse.java
@@ -2,9 +2,11 @@ package com.mybank.atmweb.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
 public class ApiResponse<T>{
