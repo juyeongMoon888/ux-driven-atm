f7189d905030dfc27823e837e2007f2f292e8a25
juyeongMoon888 <whoor2828@gmail.com>
2025-07-31 13:42:52 +0900

    fix: 재발급된 refreshToken을 Redis에 저장하지 않아 유효성 검사 실패하던 문제 수정

--- src/main/java/com/mybank/atmweb/controller/AuthApiController.java
+++ src/main/java/com/mybank/atmweb/controller/AuthApiController.java
@@ -23,12 +23,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import static com.mybank.atmweb.service.AuthService.ACCESS_TOKEN_PREFIX;
 import static com.mybank.atmweb.service.AuthService.REFRESH_TOKEN_PREFIX;
 
 @Slf4j
@@ -70,25 +74,39 @@ public class AuthApiController {
     }
 
     @PostMapping("/token/refresh")
-    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("✅refreshToken 진입");
         Cookie[] cookies = request.getCookies();
         String refreshToken = authService.findRefreshTokenFromCookies(cookies);
 
+        //1. 쿠키에 refreshToken이 존재하지 않으면 재발급 불가
+        if (refreshToken == null) {
+            return responseUtil.buildResponse(ErrorCode.TOKEN_INVALID, HttpStatus.UNAUTHORIZED, null);
+        }
+
         try {
+            //2. 토큰 유효성 검증 (만료, 위조 등 체크)
             jwtUtil.validateToken(refreshToken);
         } catch (ExpiredJwtException e) {
             //토큰 만료된 경우
+            System.out.println("ExpiredJwtException 발생");
             throw new CustomException(ErrorCode.TOKEN_EXPIRED);
         } catch (MalformedJwtException e) {
             //토큰 구조가 잘못된 경우
+            System.out.println("MalformedJwtException 발생");
             throw new CustomException(ErrorCode.TOKEN_MALFORMED);
         } catch (JwtException e) {
             // 그 외 위조된 경우 등
+            System.out.println("JwtException 발생");
             throw new CustomException(ErrorCode.TOKEN_INVALID);
         }
 
+        // 3. userId 추출
         Long userId = jwtUtil.getUserId(refreshToken);
+        System.out.println("✅userId = " + userId);
 
+
+        // 4. Redis 저장값과 비교
         String savedToken = redisTemplate.opsForValue().get(REFRESH_TOKEN_PREFIX + userId);
         if (savedToken == null || !refreshToken.equals(savedToken)) {
             return responseUtil.buildResponse(ErrorCode.TOKEN_INVALID, HttpStatus.UNAUTHORIZED, null);
@@ -98,6 +116,23 @@ public class AuthApiController {
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
         String newAccessToken = jwtUtil.createAccessToken(user);
+        System.out.println("🔥newAccessToken = " + newAccessToken);
+
+        //5. 재발급 토큰을 redis에 저장
+        long newAccessTokenTtl = jwtUtil.getExpirationMillis(newAccessToken);
+        redisTemplate.opsForValue().set(
+                ACCESS_TOKEN_PREFIX + user.getId(),
+                newAccessToken,
+                Duration.ofMillis(newAccessTokenTtl)
+        );
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", newAccessToken)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(Duration.ofMillis(jwtUtil.getExpirationMillis(newAccessToken)))
+                .build();
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
 
         return ResponseEntity.ok(Map.of(
                 "code", SuccessCode.ACCESS_TOKEN_REISSUED.name(),
