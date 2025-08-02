71b96352a5e7244f00220a1094625dd240f9052f
juyeongMoon888 <whoor2828@gmail.com>
2025-08-02 19:17:46 +0900

    fix: BankType enum 필드 name 제거 및 상수명 수정으로 400 오류 해결

--- src/main/java/com/mybank/atmweb/domain/BankType.java
+++ src/main/java/com/mybank/atmweb/domain/BankType.java
@@ -1,22 +1,16 @@
 package com.mybank.atmweb.domain;
 
 public enum BankType {
-    A("KB","국민은행", "110"),
-    B("SHINHAN","신한은행", "120"),
-    C("WOORI","우리은행", "333");
+    KB("국민은행", "110"),
+    SHINHAN("신한은행", "120"),
+    WOORI("우리은행", "333");
 
-    private final String name;
     private final String displayName;
     private final String prefix;
 
-    BankType(String name, String displayName, String prefix) {
+    BankType(String displayName, String prefix) {
         this.displayName = displayName;
         this.prefix = prefix;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getDisplayName() {
