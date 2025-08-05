6ab8524b94f9ff9356ed4dd982cfd4c7201ab71f
juyeongMoon888 <whoor2828@gmail.com>
2025-08-06 00:16:53 +0900

    style: 에러 메시지 상수명을 ErrorCode로 변경하여 일관성 향상

--- src/main/resources/static/js/lib/constants/errorMessages.js
+++ src/main/resources/static/js/lib/constants/errorMessages.js
@@ -1,20 +1,19 @@
-export const ERROR_MESSAGES = {
+export const ErrorCode = {
   NOTBLANK_LOGINID: "아이디를 입력해주세요",
-
   NOTBLANK_PASSWORD: "비밀번호를 입력해주세요",
   SIZE_PASSWORD: "비밀번호는 8자 이상이어야 합니다",
-
   NOTBLANK_EMAIL: "이메일을 입력해주세요",
   EMAIL_EMAIL: "이메일 형식이 올바르지 않습니다",
-
   NOTBLANK_NAME: "이름을 입력해주세요",
-
   NOTBLANK_RESIDENTNUMBER: "주민등록번호를 입력해주세요",
   SIZE_RESIDENTNUMBER: "주민등록번호는 13자리 숫자여야 합니다",
-
   NOTBLANK_GENDER: "성별을 입력해주세요",
-
   NOTBLANK_PHONENUMBER: "전화번호를 입력해주세요",
   PATTERN_PHONENUMBER: "전화번호는 10~11자리 숫자로 입력해주세요",
+
+  VALIDATION_FAILED: "입력값이 올바르지 않습니다.",
+  TOKEN_EXPIRED: "세션이 만료되었습니다. 다시 로그인해주세요.",
+  SERVER_ERROR: "서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.",
+  NETWORK_DOWN: "서버가 꺼졌습니다. 복구를 기다립니다...",
   UNKNOWN_ERROR: "알 수 없는 오류가 발생했습니다"
 };
