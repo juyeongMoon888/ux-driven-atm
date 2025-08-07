6009d3b5c5d1419d9b7527c83727ecad2a043109
juyeongMoon888 <whoor2828@gmail.com>
2025-08-07 13:03:33 +0900

    fix: 누락된 파일 import 추가

--- src/main/resources/static/js/lib/network/handleNetworkOrApiError.js
+++ src/main/resources/static/js/lib/network/handleNetworkOrApiError.js
@@ -1,10 +1,22 @@
+import { showFieldErrors } from "/js/lib/validation/renderFieldError.js";
+import { ApiError } from "/js/errors/ApiError.js";
+
 export function handleNetworkOrApiError(err) {
     if (err instanceof ApiError) {
         if (err.code === "TOKEN_EXPIRED") {
             alert(ErrorCode.TOKEN_EXPIRED);
             location.href = "/login";
         } else if (err.code === "VALIDATION_FAILED") {
-            showErrorMessagesFromServer(parsed.data);
+            showFieldErrors()
+        } else if (err.code == "INVALID_CREDENTIALS") {
+            alert(ErrorCode.INVALID_CREDENTIALS);
+            location.href = "/login";
+        } else if (err.code == "TOKEN_NOT_FOUND") {
+            alert(ErrorCode.TOKEN_NOT_FOUND);
+            location.href = "/login";
+        } else if (err.code == "BANK_INVALID") {
+            const errors = {bankSelect: "BANK_INVALID"};
+            showFieldErrors(errors, [bankSelect]);
         }
     } else if (err instanceof TypeError && err.message === "Failed to fetch") {
         alert(ErrorCode.NETWORK_DOWN);
