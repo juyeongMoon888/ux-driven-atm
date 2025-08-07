37d54a3b602b8823bb8c31e39330e5055b9c2041
juyeongMoon888 <whoor2828@gmail.com>
2025-08-07 11:24:59 +0900

    refactor: index.js 중복 제거 및 오류 처리 함수화

--- src/main/resources/static/js/bank/index.js
+++ src/main/resources/static/js/bank/index.js
@@ -13,18 +13,17 @@ async function main() {
 function initElement() {
     bankPage = document.getElementById("bankPage");
     accountCreateBtn = document.getElementById("accountCreateBtn");
-    console.log("✅ accountCreateBtn:", accountCreateBtn);
 }
 
 function bindEvents() {
     if (accountCreateBtn) {
-        console.log("✅ 이벤트 바인딩 시작됨");
         accountCreateBtn.addEventListener("click", handleAccountCreate);
     } else {
         console.warn("❗ accountCreateBtn이 DOM에 없음");
         console.log("accountCreateBtn이 생성되지 않았습니다.")
     }
 }
+
 async function handleAccountCreate() {
     location.href = "/bank/open-account";
 }
@@ -34,17 +33,14 @@ async function checkTokenBeforeEnteringBank() {
             method: "GET",
             credentials: "include"
         })
+        const parsed = await fetchJsonSafe(res);
 
         if (res.ok) {
             return true;
         } else {
-            alert("로그인 후 이용 가능한 서비스입니다.")
-            location.href = "/login";
-            return;
+            handleApiFailure(res, parsed);
         }
     } catch (err) {
-        console.log("요청 실패", err);
-        location.href = "/login";
-        return false;
+        handleNetworkOrApiError(err);
     }
 }
