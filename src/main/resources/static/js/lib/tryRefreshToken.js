56e198396cdeb2d6b461df92865195a9bb271aec
juyeongMoon888 <whoor2828@gmail.com>
2025-08-08 16:41:22 +0900

    refactor: accessToken 없을 때 조기 반환하여 코드 흐름 단순화

--- src/main/resources/static/js/lib/tryRefreshToken.js
+++ src/main/resources/static/js/lib/tryRefreshToken.js
@@ -1,4 +1,5 @@
 export async function tryRefreshToken() {
+console.log("tryRefreshToken 진입");
 let accessToken;
     try {
         const res = await fetch("/api/auth/token/refresh", {
@@ -7,18 +8,16 @@ let accessToken;
         });
 
         if (!res.ok) {
-        console.warn("❌ 토큰 재발급 실패", res.status);
             throw new Error("토큰 갱신 실패");
         }
         const parsed = await res.json();
 
         accessToken = parsed.data?.accessToken;
-        if (accessToken) {
-            localStorage.setItem("accessToken", accessToken);
-                    console.log("✅ 토큰 재발급 성공:", localStorage.getItem("accessToken"));
-        } else {
+        if (!accessToken) {
             console.warn("⚠️ accessToken이 응답에 없습니다:", parsed);
+            return null;
         }
+        localStorage.setItem("accessToken", accessToken);
         return accessToken;
     } catch (err) {
         console.error("토큰 재발급 중 예외 발생:", err);
