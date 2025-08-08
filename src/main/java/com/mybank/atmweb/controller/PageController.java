659b9c403b4f5b97f719eacfe6661386edabe54e
juyeongMoon888 <whoor2828@gmail.com>
2025-08-08 17:03:18 +0900

    계좌 목록 및 입출금 화면 반환하는 뷰 컨트롤러 메서드 추가

--- src/main/java/com/mybank/atmweb/controller/PageController.java
+++ src/main/java/com/mybank/atmweb/controller/PageController.java
@@ -1,17 +1,17 @@
 package com.mybank.atmweb.controller;
 
 import com.mybank.atmweb.domain.BankType;
+import com.mybank.atmweb.dto.AccountSummaryDto;
 import com.mybank.atmweb.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class PageController {
-
-    private final UserService userService;
 
     @GetMapping("/")
     public String mainPage() {
@@ -30,7 +30,7 @@ public class PageController {
 
     @GetMapping("/bank")
     public String bankPage() {
-        return "bank";
+        return "bank/index";
     }
 
     @GetMapping("/bank/open-account")
@@ -39,7 +39,17 @@ public class PageController {
         return "bank/open-account";
     }
 
-    //입출금
+    @GetMapping("/bank/accounts")
+    public String accountList() {
+        return "bank/account-list";
+    }
+
+    @GetMapping("/bank/transfer")
+    public String transfer(@RequestParam String accountNumber, Model model) {
+        model.addAttribute("accountNumber", accountNumber);
+        return "bank/transfer";
+    }
+
     @GetMapping("/transactions")
     public String transactionForm() {
         return "transactions";
