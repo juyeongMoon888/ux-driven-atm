35b02fe393e8e28591b549ac31a2f600d0342d72
juyeongMoon888 <whoor2828@gmail.com>
2025-08-08 16:57:17 +0900

    feat: 사용자 계좌 목록 조회용 accountList API 구현

--- src/main/java/com/mybank/atmweb/controller/AccountApiController.java
+++ src/main/java/com/mybank/atmweb/controller/AccountApiController.java
@@ -2,18 +2,22 @@ package com.mybank.atmweb.controller;
 
 import com.mybank.atmweb.auth.JwtUtil;
 import com.mybank.atmweb.dto.AccountRequestDto;
+import com.mybank.atmweb.dto.AccountSummaryDto;
 import com.mybank.atmweb.global.ResponseUtil;
+import com.mybank.atmweb.global.code.ErrorCode;
 import com.mybank.atmweb.global.code.SuccessCode;
+import com.mybank.atmweb.global.exception.user.CustomException;
 import com.mybank.atmweb.service.AccountService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/bank")
@@ -33,4 +37,19 @@ public class AccountApiController {
 
         return responseUtil.buildResponse(SuccessCode.ACCOUNT_CREATED, HttpStatus.OK, null);
     }
+
+    @GetMapping("/account-list")
+    public ResponseEntity<?> accountList(HttpServletRequest request) {
+        try {
+            String token = jwtUtil.extractToken(request);
+            Long userId = jwtUtil.getUserId(token);
+
+            List<AccountSummaryDto> accountList = accountService.getAccountSummariesByUserId(userId);
+
+            return responseUtil.buildResponse(SuccessCode.READ_SUCCESS, HttpStatus.OK, accountList);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new CustomException(ErrorCode.TOKEN_INVALID);
+        }
+    }
+
 }
