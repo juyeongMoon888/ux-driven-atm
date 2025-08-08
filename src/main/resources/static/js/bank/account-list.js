3b4d4e673344db5c6b717524cc2636e69e161cc5
juyeongMoon888 <whoor2828@gmail.com>
2025-08-08 18:25:30 +0900

    feat: 계좌 목록 동적 렌더링 및 버튼 클릭 이벤트 처리

---
+++ src/main/resources/static/js/bank/account-list.js
@@ -0,0 +1,71 @@
+import { fetchWithAuth } from "/js/lib/fetchWithAuth.js";
+import { fetchJsonSafe } from "/js/lib/fetchJsonSafe.js";
+import { handleNetworkOrApiError } from "/js/lib/network/handleNetworkOrApiError.js";
+import { handleApiFailure } from "/js/lib/api/handleApiFailure.js";
+
+
+document.addEventListener("DOMContentLoaded", main);
+let myAccountList, bankType, accountNumber, balance;
+function main() {
+    initElement();
+    bindEvents();
+    myAccount();
+}
+
+function initElement() {
+    myAccountList = document.getElementById("myAccountList");
+    bankType = document.getElementById("bankType");
+    accountNumber = document.getElementById("accountNumber");
+    balance = document.getElementById("balance");
+}
+function bindEvents() {
+    bindDynamicButtonEvents();
+}
+async function myAccount() {
+    console.log("myAccount 진입");
+    let res, parsed;
+
+    try {
+        res = await fetchWithAuth("/api/bank/account-list", {
+            method: "GET",
+            credentials: "include"
+        });
+        parsed = await fetchJsonSafe(res);
+
+        const accounts = parsed.data;
+        if (res.ok) {
+            if (Array.isArray(accounts)) {
+                for (const account of accounts) {
+                    const accountDiv = document.createElement("div");
+                    accountDiv.innerHTML = `
+                        <p>은행명: ${account.bankType}</p>
+                        <p>계좌번호: ${account.accountNumber}</p>
+                        <p>잔고: ${account.balance}</p>
+                        <button class = "btn btn-primary transferBtn" data-account="${account.accountNumber}">입출금</button>
+                        <button class = "btn btn-primary historyBtn" data-account="${account.accountNumber}">거래내역 보기</button>
+                        <hr/>
+                    `;
+                    myAccountList.appendChild(accountDiv);
+                }
+
+                localStorage.setItem("account", JSON.stringify(accounts));
+                return true;
+            }
+        }else {
+            handleApiFailure(res, parsed);
+        }
+    } catch (err) {
+        handleNetworkOrApiError(err);
+    }
+}
+
+function bindDynamicButtonEvents() {
+    document.addEventListener("click", (e) => {
+        if (e.target.classList.contains("transferBtn")) {
+            const accountNumber = e.target.dataset.account;
+            console.log("선택된 계좌번호:", accountNumber);
+
+            location.href = `/bank/transfer?accountNumber=${accountNumber}`;
+        }
+    });
+}
