e31fa3beeac3755d8ef22a3c5255fef80999fe31
juyeongMoon888 <whoor2828@gmail.com>
2025-08-08 18:22:41 +0900

    feat: 사용자 ID로 계좌 목록 조회하는 JPA 메서드 findByOwner_Id 추가

--- src/main/java/com/mybank/atmweb/repository/AccountRepository.java
+++ src/main/java/com/mybank/atmweb/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.mybank.atmweb.repository;
 import com.mybank.atmweb.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Set<Account> findByOwner_Id(Long userId);
 }
