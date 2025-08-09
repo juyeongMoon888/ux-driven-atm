1884d9fbae750bf7933addcefefa53a4662deaf0
juyeongMoon888 <whoor2828@gmail.com>
2025-08-09 15:16:02 +0900

    fix: getUserFromLocalStorage에서 파싱 실패 시 로컬스토리지 데이터 제거하도록 수정

--- src/main/resources/static/js/lib/storage/getUserFromLocalStorage.js
+++ src/main/resources/static/js/lib/storage/getUserFromLocalStorage.js
@@ -1,4 +1,10 @@
 export function getUserFromLocalStorage() {
     const userJson = localStorage.getItem("user");
-    return userJson ? JSON.parse(userJson) : null;
+    try {
+        return userJson ? JSON.parse(userJson) : null;
+    } catch (err) {
+        console.warn("로컬스토리지 파싱 실패, 제거합니다");
+        localStorage.removeItem("user");
+        return null;
+    }
 }
