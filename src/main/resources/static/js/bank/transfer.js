0b415b4981d481cd8031b19125885402e169d799
juyeongMoon888 <whoor2828@gmail.com>
2025-08-09 13:44:25 +0900

    feat: 입출금 화면 동작 처리용 transfer.js 파일 추가

---
+++ src/main/resources/static/js/bank/transfer.js
@@ -0,0 +1,58 @@
+import { fetchWithAuth } from "/js/lib/fetchWithAuth.js";
+import { fetchJsonSafe } from "/js/lib/fetchJsonSafe.js";
+import { handleApiFailure } from "/js/lib/api/handleApiFailure.js"
+import { handleNetworkOrApiError } from "/js/lib/network/handleNetworkOrApiError.js"
+
+document.addEventListener("DOMContentLoaded", main);
+let transferForm, amountInput;
+
+function main() {
+    initElement();
+    bindEvents();
+}
+
+function initElement() {
+    transferForm = document.getElementById("transferForm");
+    amountInput = document.getElementById("amount");
+}
+
+function bindEvents() {
+    if (transferForm) {
+        transferForm.addEventListener("submit", handleTransferProgress);
+    } else {
+        console.warn("❗ transferForm이 DOM에 없음");
+    }
+}
+
+async function handleTransferProgress(e) {
+    e.preventDefault();
+
+    let res, parsed;
+    const type = document.querySelector('input[name="type"]:checked').value;
+    const accountNumber = transferForm.dataset.accountNumber;
+    const amount = amountInput.value;
+
+    try {
+        res = await fetchWithAuth("/api/bank/transfer", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                type, accountNumber, amount
+            })
+        });
+        parsed = await fetchJsonSafe(res);
+
+        if (res.ok) {
+            location.href = "/bank/accounts";
+            return true;
+        } else {
+            handleApiFailure(res, parsed);
+        }
+
+    } catch (err) {
+        handleNetworkOrApiError(err);
+    }
+}
