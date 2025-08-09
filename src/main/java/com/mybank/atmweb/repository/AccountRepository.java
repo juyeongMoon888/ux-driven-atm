224b7be493acf29c744a1009c44aa838d9747ca3
juyeongMoon888 <whoor2828@gmail.com>
2025-08-09 14:17:53 +0900

    feat: AccountRepository에 계좌번호와 사용자 ID로 계좌 조회 메서드 추가

--- src/main/java/com/mybank/atmweb/repository/AccountRepository.java
+++ src/main/java/com/mybank/atmweb/repository/AccountRepository.java
@@ -3,8 +3,11 @@ package com.mybank.atmweb.repository;
 import com.mybank.atmweb.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Set<Account> findByOwner_Id(Long userId);
+
+    Optional<Account> findByAccountNumberAndOwner_Id(String accountNumber, Long userId);
 }
