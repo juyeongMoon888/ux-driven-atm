153746170acd61658db889c0bd7a771713646e31
juyeongMoon888 <whoor2828@gmail.com>
2025-08-16 14:49:15 +0900

    refactor: Service에 주민등록번호 포맷팅 메서드 추가

--- src/main/java/com/mybank/atmweb/service/UserService.java
+++ src/main/java/com/mybank/atmweb/service/UserService.java
@@ -2,18 +2,23 @@ package com.mybank.atmweb.service;
 
 import com.mybank.atmweb.domain.Role;
 import com.mybank.atmweb.domain.User;
+import com.mybank.atmweb.domain.UserProfile;
 import com.mybank.atmweb.dto.SignupForm;
 import com.mybank.atmweb.global.code.ErrorCode;
 import com.mybank.atmweb.global.exception.user.CustomException;
+import com.mybank.atmweb.repository.UserProfileRepository;
 import com.mybank.atmweb.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserProfileRepository userProfileRepository;
 
     public void signup(SignupForm form) {
         //아이디 중복 확인 버튼과 별개로 아이디 중복 체크
@@ -25,16 +30,32 @@ public class UserService {
         User user = new User();
         user.setLoginId(form.getLoginId());
         user.setPassword(passwordEncoder.encode(form.getPassword()));
-        user.setName(form.getName());
+        user.setName(formatResidentNumber(form.getName()));
         user.setResidentNumber(form.getResidentNumber());
         user.setGender(form.getGender());
         user.setPhoneNumber(form.getPhoneNumber());
         user.setRole(Role.USER);
 
         userRepository.save(user);
+
+        //UserProfile
+        UserProfile profile = new UserProfile();
+        profile.setUser(user);
+        profile.setName(user.getName());
+        profile.setBirth(user.getResidentNumber());
+        userProfileRepository.save(profile);
+
     }
     //아이디 중복 확인
     public boolean isDuplicatedLoginId(String loginId) {
         return userRepository.findByLoginId(loginId).isPresent();
+    }
+
+    private String formatResidentNumber(String rn) {
+        if (rn != null && rn.length() == 13) {
+            return rn.substring(0, 6) + "-" + rn.substring(6);
+        }
+        throw new IllegalArgumentException("주민번호 형식 오류");
+
     }
 }
