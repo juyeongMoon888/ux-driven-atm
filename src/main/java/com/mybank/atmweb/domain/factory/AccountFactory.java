2430ac2db61496e4ec84dc78ecc7395a9ac89102
juyeongMoon888 <whoor2828@gmail.com>
2025-08-18 12:18:09 +0900

    feat: Account 엔티티 생성을 담당하는 AccountFactory 클래스 추가

---
+++ src/main/java/com/mybank/atmweb/domain/factory/AccountFactory.java
@@ -0,0 +1,24 @@
+package com.mybank.atmweb.domain.factory;
+
+import com.mybank.atmweb.domain.Account;
+import com.mybank.atmweb.domain.BankType;
+import com.mybank.atmweb.domain.User;
+import com.mybank.atmweb.dto.ExternalAccountOpenResponseDto;
+import jakarta.persistence.Entity;
+
+public class AccountFactory {
+    private AccountFactory() {
+
+    }
+
+    public static Account fromExternalResponse(User user, ExternalAccountOpenResponseDto data) {
+        return Account.builder()
+                .owner(user)
+                .bank(BankType.valueOf(data.getBankType()))
+                .accountNumber(data.getAccountNumber())
+                .accountName(data.getAccountName())
+                .externalAccountId(data.getExternalAccountId())
+                .balance(data.getBalance())
+                .build();
+    }
+}
