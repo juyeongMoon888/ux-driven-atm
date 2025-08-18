8313741ebfc3d65cd6b570350a33f6c464515e3f
juyeongMoon888 <whoor2828@gmail.com>
2025-08-18 11:11:22 +0900

    feat: UserProfile 엔티티용 Repository 인터페이스 추가

---
+++ src/main/java/com/mybank/atmweb/repository/UserProfileRepository.java
@@ -0,0 +1,12 @@
+package com.mybank.atmweb.repository;
+
+import com.mybank.atmweb.domain.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUserId(Long userId);
+}
