61bb328ac608192192db49f592c76015abf1df67
juyeongMoon888 <whoor2828@gmail.com>
2025-08-19 08:13:20 +0900

    feat: Transaction 관련 조회 기능 TransactionQueryService으로 분리

---
+++ src/main/java/com/mybank/atmweb/application/TransactionQueryService.java
@@ -0,0 +1,21 @@
+package com.mybank.atmweb.application;
+
+import com.mybank.atmweb.domain.transaction.Transaction;
+import com.mybank.atmweb.global.code.ErrorCode;
+import com.mybank.atmweb.global.exception.user.CustomException;
+import com.mybank.atmweb.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionQueryService {
+    private final TransactionRepository transactionRepository;
+
+    @Transactional
+    public Transaction getTransactionDetailOrThrow(Long transactionId, Long userId) {
+        return transactionRepository.findByIdAndAccount_Owner_Id(transactionId, userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_DETAIL_NOT_FOUND));
+    }
+}
