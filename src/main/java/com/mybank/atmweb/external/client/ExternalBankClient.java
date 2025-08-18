cce8616f696d4229c9fbb72becf0752458bb1264
juyeongMoon888 <whoor2828@gmail.com>
2025-08-18 10:50:11 +0900

    feat: ExternalBankClient#createAccount 추가 - 외부 은행 계좌개설 API 연동

--- src/main/java/com/mybank/atmweb/external/client/ExternalBankClient.java
+++ src/main/java/com/mybank/atmweb/external/client/ExternalBankClient.java
@@ -1,30 +1,36 @@
 package com.mybank.atmweb.external.client;
 
-import com.mybank.atmweb.dto.AccountOpenRequestDto;
-import com.mybank.atmweb.dto.ExternalAccountOpenResponse;
+import com.mybank.atmweb.dto.ApiResponse;
+import com.mybank.atmweb.dto.ExternalAccountOpenResponseDto;
 import com.mybank.atmweb.dto.ExternalOpenAccountRequestDto;
-import com.mybank.atmweb.dto.UserProfile;
 import com.mybank.atmweb.external.dto.ExternalAccountVerifyResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ExternalBankClient {
     private final RestTemplate restTemplate;
 
-    public ExternalAccountOpenResponse createAccount(ExternalOpenAccountRequestDto dto) {
+    public ApiResponse<ExternalAccountOpenResponseDto> createAccount(ExternalOpenAccountRequestDto dto) {
+        log.info("ExternalBankClient 진입");
         String url = "http://localhost:8081/api/external-bank/open-account";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<ExternalOpenAccountRequestDto> entity = new HttpEntity<>(dto, headers);
-        ResponseEntity<ExternalAccountOpenResponse> res =
-                restTemplate.exchange(url, HttpMethod.POST, entity, ExternalAccountOpenResponse.class);
-        return res.getBody();
+
+        ResponseEntity<ApiResponse<ExternalAccountOpenResponseDto>> ress =
+                restTemplate.exchange(url, HttpMethod.POST, entity, new ParameterizedTypeReference<>() {
+                });
+
+        return ress.getBody();
+
     }
 
     public ExternalAccountVerifyResponse verifyAccount(String bankType, String accountNumber) {
