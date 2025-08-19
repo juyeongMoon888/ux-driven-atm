ef3650af8b27340641663075420b18167151a162
juyeongMoon888 <whoor2828@gmail.com>
2025-08-19 15:44:11 +0900

    feat: 사용자 조회 로직 추가(getByIdOrThrow)

--- src/main/java/com/mybank/atmweb/application/UserQueryService.java
+++ src/main/java/com/mybank/atmweb/application/UserQueryService.java
@@ -1,4 +1,20 @@
 package com.mybank.atmweb.application;
 
+import com.mybank.atmweb.domain.user.User;
+import com.mybank.atmweb.global.code.ErrorCode;
+import com.mybank.atmweb.global.exception.user.CustomException;
+import com.mybank.atmweb.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserQueryService {
+    private final UserRepository userRepository;
+
+    public User getByIdOrThrow(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
 }
