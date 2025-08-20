bab360f5b2cd71e51d4b94dd96a48ed146ab1fc6
juyeongMoon888 <whoor2828@gmail.com>
2025-08-20 11:15:40 +0900

    refactor: TransactionRepository 메서드 JPQL 프로젝션으로 수정, DTO 반환

--- src/main/java/com/mybank/atmweb/repository/TransactionRepository.java
+++ src/main/java/com/mybank/atmweb/repository/TransactionRepository.java
@@ -1,16 +1,46 @@
 package com.mybank.atmweb.repository;
 
-import com.mybank.atmweb.domain.Transaction;
+import com.mybank.atmweb.domain.transaction.Transaction;
+import com.mybank.atmweb.dto.TransactionDetailSummaryDto;
+import com.mybank.atmweb.dto.TransactionSummaryDto;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    Set<Transaction> findByAccount_AccountNumberAndAccount_Owner_IdOrderByCreatedAtDesc(String accountNumber, Long userId);
-
     Optional<Transaction> findByIdAndAccount_Owner_Id(Long transactionId, Long userId);
+
+    @Query("""
+            select new com.mybank.atmweb.dto.TransactionSummaryDto(
+                t.id, t.createdAt, t.transfer, t.amount, t.memo
+            )
+            from Transaction t
+            where t.account.accountNumber = :accountNumber
+            and t.account.owner.id = :userId
+            order by t.createdAt desc
+            """)
+    List<TransactionSummaryDto> findSummariesByAccountNumberAndUserId(
+            @Param("accountNumber") String accountNumber,
+            @Param("userId") Long userId
+    );
+
+    @Query("""
+            select new com.mybank.atmweb.dto.TransactionDetailSummaryDto(
+                t.createdAt, t.transfer, t.amount, t.balanceAfter, t.memo
+            )
+            from Transaction t
+            where t.id = :transactionId
+            and t.account.owner.id = :userId
+            """)
+    TransactionDetailSummaryDto findHistoryDetailByTransactionIdAndUserId(
+            @Param("transactionId") Long transactionId,
+            @Param("userId") Long userId
+    );
 }
