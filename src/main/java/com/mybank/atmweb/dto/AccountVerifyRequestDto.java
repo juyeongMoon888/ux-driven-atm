8aa08141e2f91d533a7f3cc6ab15eb5e60f79a32
juyeongMoon888 <whoor2828@gmail.com>
2025-08-21 09:59:04 +0900

    refactor: 계좌 검증 DTO 통합 (internal/external -> 단일 DTO)

---
+++ src/main/java/com/mybank/atmweb/dto/AccountVerifyRequestDto.java
@@ -0,0 +1,12 @@
+package com.mybank.atmweb.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ExternalAccountVerifyRequestDto {
+    private final String bank;
+    private final String accountNumber;
+}
+
