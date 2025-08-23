f35b8a823478305aaa0b2aab06e6d31acb92c81b
juyeongMoon888 <whoor2828@gmail.com>
2025-08-23 15:33:57 +0900

    feat: 계좌 검증 및 금액 입력란 노출 기능(handleAccountVerify) 구현

--- src/main/resources/static/js/bank/transfer.js
+++ src/main/resources/static/js/bank/transfer.js
@@ -1,49 +1,137 @@
+import { validateUser } from "/js/lib/validation/validateUser.js";
+import { showFieldErrors } from "/js/lib/validation/renderFieldError.js";
 import { fetchWithAuth } from "/js/lib/fetchWithAuth.js";
 import { fetchJsonSafe } from "/js/lib/fetchJsonSafe.js";
 import { handleApiFailure } from "/js/lib/api/handleApiFailure.js"
 import { handleNetworkOrApiError } from "/js/lib/network/handleNetworkOrApiError.js"
 
 document.addEventListener("DOMContentLoaded", main);
-let transferForm, amountInput, memoInput;
+let externalTransferFormEl, verifyBtnEl, bankSelectEl, fromAccountSelectEl, toAccountNumberEl, amountEl, memoEl, checkResultEl, transferBtnEl, amountSectionEl;
 
 function main() {
     initElement();
+    handleFromAccountSelect();
     bindEvents();
 }
 
 function initElement() {
-    transferForm = document.getElementById("transferForm");
-    amountInput = document.getElementById("amount");
-    memoInput = document.getElementById("memo");
+    externalTransferFormEl = document.getElementById("externalTransferForm");
+    verifyBtnEl = document.getElementById("verifyBtn");
+    bankSelectEl = document.getElementById("bankSelect");
+    fromAccountSelectEl = document.getElementById("fromAccountNumber");
+    toAccountNumberEl = document.getElementById("toAccountNumber");
+    amountEl = document.getElementById("amount");
+    memoEl = document.getElementById("memo");
+    transferBtnEl = document.getElementById("transferBtn");
+    amountSectionEl = document.getElementById("amountSection");
 }
 
 function bindEvents() {
-    if (transferForm) {
-        transferForm.addEventListener("submit", handleTransferProgress);
+    if (verifyBtn) {
+        verifyBtn.addEventListener("click", handleAccountVerify);
     } else {
-        console.warn("❗ transferForm이 DOM에 없음");
+        console.warn("verifyBtn DOM에 없음");
+    }
+
+    if (externalTransferFormEl) {
+        externalTransferFormEl.addEventListener("submit", handleExternalTransfer);
+    } else {
+        console.warn("externalTransferFormEl DOM에 없음");
+    }
+
+    if (transferBtnEl) {
+        transferBtnEl.addEventListener("click", handleExternalTransfer);
+    } else {
+        console.warn("transferBtnEl DOM에 없음");
     }
 }
 
-async function handleTransferProgress(e) {
+async function handleFromAccountSelect() {
+    try {
+        const res = await fetchWithAuth("/api/bank/account-options", {
+                method: "GET",
+                credentials: "include"
+        });
+        const parsed = await fetchJsonSafe(res);
+        const accounts = parsed.data;
+        if (res.ok) {
+            const optionsHtml = (Array.isArray(accounts) ? accounts : []).map (acc =>
+            `<option value="${acc.accountNumber}">
+                [${acc.bank}] ${acc.accountNumber}
+            </option>`
+            ).join("");
+            fromAccountSelectEl.innerHTML = optionsHtml;
+        } else {
+            handleApiFailure(res, parsed);
+        }
+    } catch (err) {
+        handleNetworkOrApiError(err);
+    }
+}
+
+async function handleAccountVerify(e) {
     e.preventDefault();
+    const payload = {
+        bank: bankSelectEl.value,
+        toAccountNumber: toAccountNumberEl.value
+    }
+
+    const errors = validateUser(payload);
+    if (errors) {
+        showFieldErrors(errors, ["bank", "toAccountNumber"]);
+        return;
+    }
 
     let res, parsed;
-    const type = document.querySelector('input[name="type"]:checked').value;
-    const accountNumber = transferForm.dataset.accountNumber;
-    const amount = amountInput.value;
-    const memo = memoInput.value;
+    try {
+        res = await fetchWithAuth("/api/bank/transfer/verify", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(payload)
+        });
+
+        if (res.ok) {
+            alert("계좌검증이 완료되었습니다.");
+            verifyBtn.disabled = true;
+
+            amountSectionEl.style.display="block";
+            return true;
+        } else {
+            handleApiFailure(res, parsed);
+        }
+    } catch (err) {
+        handleNetworkOrApiError(err);
+    }
+}
+//이체 + 사용자가 현재 선택한 계좌(번호) (문자열)
+async function handleExternalTransfer(e) {
+    e.preventDefault();
+
+    //통제 변수
+    /*if(form명.dataset.idChecked !== "true") {
+        alert("계좌 검증 확인을 먼저 해주세요.");
+        return;
+    }*/
 
     try {
+        let res, parsed
+        const fromAccountNumber = fromAccountSelectEl.value;
+        const bank = bankSelectEl.value;
+        const toAccountNumber = toAccountNumberEl.value;
+        const amount = amountEl.value;
+        const memo = memoEl.value;
+        const payload = { fromAccountNumber, bank, toAccountNumber, amount, memo };
+
         res = await fetchWithAuth("/api/bank/transfer", {
             method: "POST",
             headers: {
               "Content-Type": "application/json"
             },
             credentials: "include",
-            body: JSON.stringify({
-                type, accountNumber, amount, memo
-            })
+            body: JSON.stringify(payload )
         });
         parsed = await fetchJsonSafe(res);
 
@@ -53,7 +141,6 @@ async function handleTransferProgress(e) {
         } else {
             handleApiFailure(res, parsed);
         }
-
     } catch (err) {
         handleNetworkOrApiError(err);
     }
