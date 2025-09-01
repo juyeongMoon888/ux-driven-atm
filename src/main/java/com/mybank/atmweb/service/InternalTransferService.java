66cf6601667dd2c1688d3967da2737f892eed015
juyeongMoon888 <whoor2828@gmail.com>
2025-09-01 09:44:03 +0900

    feat: MYBANK 내부 계좌 이체 처리용 InternalTransferService 생성

---
+++ src/main/java/com/mybank/atmweb/service/InternalTransferService.java
@@ -0,0 +1,119 @@
+package com.mybank.atmweb.service;
+
+import com.mybank.atmweb.domain.AccountStatus;
+import com.mybank.atmweb.domain.BankType;
+import com.mybank.atmweb.domain.Idempotency;
+import com.mybank.atmweb.domain.account.Account;
+import com.mybank.atmweb.domain.account.AccountRepository;
+import com.mybank.atmweb.domain.transaction.Transactions;
+import com.mybank.atmweb.dto.TransactionStatus;
+import com.mybank.atmweb.global.code.ErrorCode;
+import com.mybank.atmweb.global.code.SuccessCode;
+import com.mybank.atmweb.global.exception.user.CustomException;
+import com.mybank.atmweb.repository.IdempotencyRepository;
+import com.mybank.atmweb.repository.TransactionRepository;
+import com.mybank.atmweb.service.transfer.model.OperationContext;
+import com.mybank.atmweb.service.transfer.model.OperationSummary;
+import com.mybank.atmweb.service.transfer.model.OperationType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InternalTransferService {
+
+    private final IdempotencyRepository idemRepo;
+    private final TransactionRepository txRepo;
+    private final AccountRepository accountRepo;
+
+    public OperationSummary transferInternal(OperationContext ctx) {
+        if (idemRepo.existByKey(ctx.getIdempotencyKey())) {
+            Transactions existing = txRepo.findMasterByIdempotencyKey(ctx.getIdempotencyKey())
+                    .orElseThrow(() -> new CustomException(ErrorCode.IDEMPOTENCY_KEY_NOT_FOUND));
+            return new OperationSummary(SuccessCode.TRANSFER_OK.name(), SuccessCode.TRANSFER_OK.getMessageKey(),
+            TransactionStatus.COMPLETED, existing.getId());
+        }
+
+        long fromId = accountRepo.findIdByAccountNumber(ctx.getFromAccountNumber())
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND));
+        long toId = accountRepo.findIdByAccountNumber(ctx.getToAccountNumber())
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        long firstId = Math.min(fromId, toId);
+        long secondId = Math.max(fromId, toId);
+
+        // 락
+        Account first = accountRepo.findById(firstId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND));
+        Account second = accountRepo.findById(secondId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        // 의미 매핑
+        Account from = first.getAccountName().equals(ctx.getFromAccountNumber()) ? first : second;
+        Account to = from == first ? second : from;
+
+        // 검증 1 - 로직
+        validateSameBankMybank(from, to);
+
+        // 검증 2 - 유효한 계좌 상태
+        validateAccountUsable(from, to);
+
+        // 검증 3 - 이체 송금인
+        validateOwnership(from, ctx.getUserId());
+
+        // 검증 4 - 충분한 잔액
+        ensureSufficientBalance(from, ctx.getAmount());
+
+        long fromBefore = from.getBalance();
+        long toBefore = from.getBalance();
+
+        from.withdraw(ctx.getAmount());
+        to.deposit(ctx.getAmount());
+
+
+        Transactions tx = txRepo.save(
+                Transactions.builder()
+                        .account(from)
+                        .operationType(OperationType.WITHDRAW)
+                        .amount(ctx.getAmount())
+                        .balanceBefore(fromBefore)
+                        .balanceAfter(from.getBalance())
+                        .memo(ctx.getMemo())
+                        .fromBank(BankType.valueOf(ctx.getFromBank()))
+                        .toBank(ctx.getToBank())
+                        .fromAccountNumber(ctx.getFromAccountNumber())
+                        .toAccountNumber(ctx.getToAccountNumber())
+                        .transactionStatus(TransactionStatus.COMPLETED)
+                        .idempotencyKey(ctx.getIdempotencyKey())
+                        .build()
+        );
+
+        idemRepo.save(new Idempotency(ctx.getIdempotencyKey(), tx.getId(), tx.getCreatedAt()));
+
+        return new OperationSummary(SuccessCode.TRANSFER_OK.name(), SuccessCode.TRANSFER_OK.getMessageKey(), TransactionStatus.COMPLETED, ctx.getUserId());
+    }
+
+    private void validateSameBankMybank(Account from, Account to) {
+        if (!from.getBank().equals(BankType.MYBANK) || !to.getBank().equals(BankType.MYBANK)) {
+            throw new CustomException(ErrorCode.UNSUPPORTED_TRANSFER);
+        }
+    }
+
+    private void validateAccountUsable(Account from, Account to) {
+        if (!from.getStatus().equals(AccountStatus.ACTIVE) || !to.getStatus().equals(AccountStatus.ACTIVE)) {
+            throw new CustomException(ErrorCode.ACCOUNT_NOT_ACTIVE);
+        }
+    }
+
+    private void validateOwnership(Account from, Long userId) {
+        if (!from.getOwner().getId().equals(userId)) {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
+    }
+
+    private void ensureSufficientBalance(Account from, long amount) {
+        if (from.getBalance() < amount) {
+            throw new CustomException(ErrorCode.INSUFFICIENT_BALANCE);
+        }
+    }
+}
