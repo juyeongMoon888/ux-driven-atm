ae52fe595a9130b43fad9fa584ffb182dfb4c54b
juyeongMoon888 <whoor2828@gmail.com>
2025-09-02 22:11:55 +0900

    feat: ExAccDepositRes에 실패 응답용 정적 팩터리 추가 (Success=false 일관화)

--- src/main/java/com/mybank/atmweb/dto/ExAccDepositRes.java
+++ src/main/java/com/mybank/atmweb/dto/ExAccDepositRes.java
@@ -3,12 +3,22 @@ package com.mybank.atmweb.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExAccDepositRes {
     private boolean success; // 입금 성공 여부
     private String code;
+    private String message;
     private long exTxId; // withdraw 단계에서 받은 exTxId
+
+    public static ExAccDepositRes fail(String code, String message) {
+        ExAccDepositRes res = new ExAccDepositRes();
+        res.setSuccess(false);
+        res.setCode(code);
+        res.setMessage(message);
+        return res;
+    }
 }
