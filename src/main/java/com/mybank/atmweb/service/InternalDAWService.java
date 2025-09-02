e8759221e9224f4c693292cc3e9d408779f12704
juyeongMoon888 <whoor2828@gmail.com>
2025-09-02 18:12:42 +0900

    feat: 내부 은행용 단순 입출금 처리 InternalDAWService 생성

---
+++ src/main/java/com/mybank/atmweb/service/InternalDAWService.java
@@ -0,0 +1,86 @@
+package com.mybank.atmweb.service;
+
+import com.mybank.atmweb.domain.AccountStatus;
+import com.mybank.atmweb.domain.BankType;
+import com.mybank.atmweb.domain.Idempotency;
+import com.mybank.atmweb.domain.account.Account;
+import com.mybank.atmweb.domain.account.AccountRepository;
+import com.mybank.atmweb.domain.transaction.Transactions;
+import com.mybank.atmweb.dto.TransactionStatus;
+import com.mybank.atmweb.global.code.ErrorCode;
+import com.mybank.atmweb.global.code.SuccessCode;
+import com.mybank.atmweb.global.exception.user.CustomException;
+import com.mybank.atmweb.repository.IdempotencyRepository;
+import com.mybank.atmweb.repository.TransactionRepository;
+import com.mybank.atmweb.service.transfer.model.OperationContext;
+import com.mybank.atmweb.service.transfer.model.OperationSummary;
+import com.mybank.atmweb.service.transfer.model.OperationType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InternalDAWService {
+
+    private final TransactionRepository txRepo;
+    private final IdempotencyRepository idemRepo;
+    private final AccountRepository accRepo;
+
+    public OperationSummary internalDeposit(OperationContext ctx) {
+
+        //멱등성
+        if (idemRepo.existByKey(ctx.getIdempotencyKey())) {
+            Transactions existing = txRepo.findByIdempotencyKey(ctx.getIdempotencyKey())
+                    .orElseThrow(() -> new CustomException(ErrorCode.IDEMPOTENCY_KEY_NOT_FOUND));
+            return new OperationSummary(SuccessCode.TRANSFER_OK.name(), SuccessCode.TRANSFER_OK.getMessageKey(),
+                    TransactionStatus.COMPLETED, existing.getId());
+        }
+
+        Account acc = accRepo.findById(ctx.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        //계좌 검증
+        validateAccountUsable(acc);
+        validateOwnership(acc, ctx.getUserId());
+
+        //입금 적용
+        long before = acc.getBalance();
+        acc.deposit(ctx.getAmount());
+
+        //트랜잭션 적용
+        Transactions tx = txRepo.save(
+                Transactions.builder()
+                        .account(acc)
+                        .operationType(OperationType.DEPOSIT)
+                        .amount(ctx.getAmount())
+                        .balanceBefore(before)
+                        .balanceAfter(acc.getBalance())
+                        .memo(ctx.getMemo())
+                        .toAccountNumber(ctx.getToAccountNumber())
+                        .toBank(ctx.getToBank())
+                        .transactionStatus(TransactionStatus.COMPLETED)
+                        .idempotencyKey(ctx.getIdempotencyKey())
+                        .build()
+        );
+
+        idemRepo.save(new Idempotency(ctx.getIdempotencyKey(), tx.getId(), tx.getCreatedAt()));
+
+        return new OperationSummary(
+                SuccessCode.DEPOSIT_OK.name(),
+                SuccessCode.DEPOSIT_OK.getMessageKey(),
+                TransactionStatus.COMPLETED,
+                ctx.getUserId());
+    }
+
+    public void validateAccountUsable(Account acc) {
+        if (!acc.getStatus().equals(AccountStatus.ACTIVE)) {
+            throw new CustomException(ErrorCode.ACCOUNT_NOT_ACTIVE);
+        }
+    }
+
+    private void validateOwnership(Account acc, Long userId) {
+        if (!acc.getOwner().getId().equals(userId)) {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
+    }
+}
