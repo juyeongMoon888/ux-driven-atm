7190ebe76cd6857bd2869876d3c85e872d4d58a9
juyeongMoon888 <whoor2828@gmail.com>
2025-09-03 15:42:12 +0900

    feat: OperationContext 도입 DTO -> 서비스 공통 컨텍스트

---
+++ src/main/java/com/mybank/atmweb/service/transfer/model/OperationContext.java
@@ -0,0 +1,21 @@
+package com.mybank.atmweb.service.transfer.model;
+
+import com.mybank.atmweb.domain.Idempotency;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class OperationContext {
+    private final OperationType operationType;
+    private final Long userId;
+    private final String fromAccountNumber; // WITHDRAW/TRANSFER 에서 사용
+    private final String toAccountNumber; // DEPOSIT/TRANSFER 에서 사용
+    private final String fromBank;
+    private final String toBank;
+    private final long amount;
+    private final String memo;
+    private final String idempotencyKey;
+}
